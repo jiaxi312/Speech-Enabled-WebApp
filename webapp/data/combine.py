a48c2ab8d75901c5a3c59025435d10c84416c337
jiaxi312 <jx0312@icloud.com>
2021-01-20 22:00:06 +0800

    "Upload codes"

--- webapp/data/combine.py
+++
@@ -1,9 +0,0 @@
-import ffmpeg
-
-import moviepy.editor as mpe
-
-my_clip = mpe.VideoFileClip('./video2.mp4')
-audio_background = mpe.AudioFileClip('./utterance_380795.wav')
-final_audio = mpe.CompositeAudioClip([my_clip.audio, audio_background])
-final_clip = my_clip.set_audio(final_audio)
-
